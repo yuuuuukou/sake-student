4c9c709849511349ab388c7da5733c225c6a5f97
yuuuuukou <yuuuuukou@gmail.com>
2022-08-14 08:19:11 +0900

    feat: クリップボードへのコピー処理追加(組み込みは未)

--- app/src/main/java/com/yuuuuukou/sakestudent/MainActivity.kt
+++ app/src/main/java/com/yuuuuukou/sakestudent/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.yuuuuukou.sakestudent
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import java.lang.Exception
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    /**
+     * クリップボードへコピー
+     * ref: https://qiita.com/CUTBOSS/items/97669c712449510fe7f0
+     */
+    fun copyToClipboard(context: Context, label: String, text: String): Boolean {
+        try {
+            val clipboardManager: ClipboardManager =
+                context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            clipboardManager.setPrimaryClip(ClipData.newPlainText(label, text))
+            return true
+        } catch (e: Exception) {
+            return false
+        }
     }
 }
